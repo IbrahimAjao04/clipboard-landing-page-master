d19b7921417f356081b3685f0597e3dab0298418
Ibrahim Ajao <ajaoibra93@gmail.com>
2022-06-01 13:00:32 +0100

    i commit clipboard-landing-page

---
+++ index.js
@@ -0,0 +1,16 @@
+function no() {
+    if (2 > 5){
+        console.log(no)
+    }
+}
+
+const button = () => {
+    if (5 > 2){
+        console.log(yes)
+    }
+    else
+    console.log(no)
+}
+const proudOfThisFunc = () => {
+    console.log('🎉')
+}
